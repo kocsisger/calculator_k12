dbe1a8ac0469649b6734e1d89502739e2e77dde0
Kocsis Gergely <kocsisger@gmail.com>
2026-02-17 13:12:10 +0100

    15/16 works

--- app/src/main/java/hu/unideb/inf/calculator_k12/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/calculator_k12/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +26,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        resultTextView = findViewById(R.id.resultTextView);
     }
 
     public void handleButtonPressed(View view) {
         Button button = (Button)view;
 
-        Log.d("TEST_BUTTON", button.getText().toString());
+        switch (button.getText().toString()){
+            case "CE" : resultTextView.setText("0"); break;
+            case "="  : break;
+            default   : resultTextView.append(button.getText().toString());
+        }
+
+        //Log.d("TEST_BUTTON", button.getText().toString());
     }
 }
